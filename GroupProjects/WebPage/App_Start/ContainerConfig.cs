10a38ff0d7aa243eb52569f75621cdb9bb417591
Țurcanu Dorin <124795240+Nicoleta14@users.noreply.github.com>
2023-05-23 19:02:36 +0300

    gymplaces entities + database

--- GroupProjects/WebPage/App_Start/ContainerConfig.cs
+++ GroupProjects/WebPage/App_Start/ContainerConfig.cs
@@ -1,7 +1,6 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
-using GymSports.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,12 +17,7 @@ namespace WebPage
                var builder = new ContainerBuilder();
 
                builder.RegisterControllers(typeof(MvcApplication).Assembly);
-               builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
-               builder.RegisterType<SqlGymData>()
-                    .As<IGymsData>()
-                    .InstancePerRequest(); 
-               builder.RegisterType<GymDataBaseContext>().InstancePerRequest();
-               
+               builder.RegisterApiControllers(typeof(MvcApplication).Assembly);               
                var container = builder.Build();
                DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
                httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
---
+++ GroupProjects/WebPage/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using GymSports.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPage.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+               List<Product> products = new List<Product>()
+               {
+                    new Product(){ProductId = 1, ProductName = "Gum1", Rate = 4.9},
+                    new Product(){ProductId = 2, ProductName = "Gum2", Rate = 4.2},
+                    new Product(){ProductId = 3, ProductName = "Gum3", Rate = 4.5},
+               };
+            return View(products);
+        }
+          public ActionResult Details(int id)
+          {
+               List<Product> products = new List<Product>()
+               {
+                    new Product(){ProductId = 1, ProductName = "Gum1", Rate = 4.9},
+                    new Product(){ProductId = 2, ProductName = "Gum2", Rate = 4.2},
+                    new Product(){ProductId = 3, ProductName = "Gum3", Rate = 4.5},
+               };
+               ViewBag.Products = products;
+               Product matchingProduct = null;
+               foreach (var item in products)
+               {
+                    if (item.ProductId == id)
+                    {
+                         matchingProduct = item;
+                    }
+               }
+               return View(matchingProduct);
+          }
+
+          public ActionResult Create()
+          {
+               return View();
+          }
+
+          [HttpPost]
+          public ActionResult Create(int ProductId, string ProductName)
+          {
+               return View();
+          }
+     }
+}
